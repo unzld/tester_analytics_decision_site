20691d12becb81699a4c6287936c6d20cf066af8
Zildjian Restituto <z-restituto@ti.com>
2020-08-05 16:06:53 +0800

    Updated TesterMonitoringTool, Added SummarizerTool
    TesterMonitoringTool
    - fixed some columns not showing proper values
    - self-test now added to calibration logs
    
    SummarizerTool
    - new tool for generating summarized .csv file
    - use -m or --merge parameter to get merged cal and diag entries
    - use -f or --file followed by path to use single log mode

---
+++ summarizertool.py
@@ -0,0 +1,150 @@
+import argparse
+from pathlib import Path
+import jsonpickle
+import os
+from testermonitoringtool import Board
+from testermonitoringtool import EntryBase
+from testermonitoringtool import CalEntry
+from testermonitoringtool import DiagEntry
+from datetime import date
+from os import mkdir
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-m','--merge', action='store_true', help='Merge cal and diag entries in summary output.')
+parser.add_argument('-f', '--file', help='Optional: summarizes the specified .LOG file instead.')
+#parser.add_argument('-f', '--file', help='Optional: summarizes the specified log file instead.')
+
+def main():
+    args = parser.parse_args()
+    board_list = []
+    
+    if args.file:
+        board_list = load_profiles()
+        gen_file(board_list, args.file)
+    else:
+        board_list = load_profiles()
+
+        if args.merge:
+            gen_csv_merged(board_list)
+        else:
+            gen_csv_cal(board_list)
+            gen_csv_diag(board_list)
+
+def load_profiles():
+    '''
+    Loads existing profiles in ./profiles and returns them as a list of Board objects
+    '''
+    board_list = []
+    for profile in Path('./profiles').iterdir():
+        if profile.is_file():
+            #load profile
+            with open(profile, 'r') as f:
+                json_str = f.read()
+                board = jsonpickle.decode(json_str)
+                #print(f'{board.name} {board.slot} {board.tester}')
+                board_list.append(board)
+    
+    print(f'{len(board_list)} board profiles loaded.')
+
+    return board_list
+
+def gen_csv_cal(board_list):
+    today = date.today().strftime('%m%d%Y')
+    filename = f'Summary_Cal_All_{today}'
+    if not Path('./output').exists():
+        try:
+            mkdir('./output')
+        except OSError:
+            print('Failed creating output directory.')
+            exit(2)
+
+    with open(f'./output/{filename}.csv', 'w') as f:
+        f.write(f'Tester,Board,Slot,DUT SN,Self-Test,Date,Time,Mode,SN,REV,Result,Remark\n')
+        for board in board_list:
+            for entry in board.cal_history:
+                f.write(f'{board.tester},{board.name},{board.slot},\t{entry.dut},{entry.self_test},\t{entry.date},\t{entry.time},{entry.mode},\t{entry.sn},{entry.rev},{entry.result},{entry.remark}\n')
+    
+    print(f'Process finished. Output located at ./output/{filename}.csv.')
+
+def gen_csv_diag(board_list):
+    today = date.today().strftime('%m%d%Y')
+    filename = f'Summary_Diag_All_{today}'
+    if not Path('./output').exists():
+        try:
+            mkdir('./output')
+        except OSError:
+            print('Failed creating output directory.')
+            exit(2)
+
+    with open(f'./output/{filename}.csv', 'w') as f:
+        f.write(f'Tester,Board,Slot,DUT Board,Date,Time,SN,Result,Remark\n')
+        for board in board_list:
+            for entry in board.diag_history:
+                f.write(f'{board.tester},{board.name},\t{board.slot},{entry.dut},\t{entry.date},\t{entry.time},\t{entry.sn},{entry.result},{entry.remark}\n')
+    
+    print(f'Process finished. Output located at ./output/{filename}.csv.')
+
+def gen_csv_merged(board_list):
+    today = date.today().strftime('%m%d%Y')
+    filename = f'Summary_Cal_Diag_All_{today}'
+    if not Path('./output').exists():
+        try:
+            mkdir('./output')
+        except OSError:
+            print('Failed creating output directory.')
+            exit(2)
+
+    with open(f'./output/{filename}.csv', 'w') as f:
+        f.write(f'Tester,Board,Slot,DUT SN,Self-Test,Date,Time,Mode,SN,REV,Result,Remark\n')
+        for board in board_list:
+            for entry in board.cal_history + board.diag_history:
+                if isinstance(entry, CalEntry):
+                    f.write(f'{board.tester},{board.name},{board.slot},\t{entry.dut},{entry.self_test},\t{entry.date},\t{entry.time},{entry.mode},\t{entry.sn},{entry.rev},{entry.result},{entry.remark}\n')
+                elif isinstance(entry, DiagEntry):
+                    f.write(f'{board.tester},{board.name},{board.slot},\t{entry.dut},N/A,\t{entry.date},\t{entry.time},Diag,\t{entry.sn},N/A,{entry.result},{entry.remark}\n')
+    print(f'Process finished. Output located at ./output/{filename}.csv.')
+
+def gen_file(board_list, filename):
+    mode = ''
+
+    if 'diag' in filename.lower():
+        mode = 'diag'
+    elif 'cal' in filename.lower():
+        mode = 'cal'
+
+    if not Path('./output').exists():
+        try:
+            mkdir('./output')
+        except OSError:
+            print('Failed creating output directory.')
+            exit(2)
+
+    empty = True
+    
+    if mode == 'cal':
+        with open(f'./output/Summary_{filename}.csv', 'w') as f:
+            f.write(f'Tester,Board,Slot,DUT SN,Self-Test,Date,Time,Mode,SN,REV,Result,Remark\n')
+            for board in board_list:
+                for entry in board.cal_history:
+                    if entry.logname.lower().endswith(f'{filename.lower()}.log'):
+                        empty = False
+                        f.write(f'{board.tester},{board.name},{board.slot},\t{entry.dut},{entry.self_test},\t{entry.date},\t{entry.time},{entry.mode},\t{entry.sn},{entry.rev},{entry.result},{entry.remark}\n')
+
+    elif mode == 'diag':
+        with open(f'./output/Summary_{filename}.csv', 'w') as f:
+            f.write(f'Tester,Board,Slot,DUT Board,Date,Time,SN,Result,Remark\n')
+            for board in board_list:
+                for entry in board.diag_history:
+                    if entry.logname.lower().endswith(f'{filename.lower()}.log'):
+                        empty = False
+                        f.write(f'{board.tester},{board.name},\t{board.slot},{entry.dut},\t{entry.date},\t{entry.time},\t{entry.sn},{entry.result},{entry.remark}\n')
+
+    if empty:
+        print(f'Process finished. No entries associated with specified filename {filename}.')
+        os.remove(f'./output/Summary_{filename}.csv')
+    else:
+        print(f'Process finished. Output located at ./output/Summary_{filename}.csv.')
+
+if __name__ == '__main__':
+    main()
